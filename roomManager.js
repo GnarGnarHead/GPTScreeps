3e7033bdd3fe1c9e076ec329c035f3b796170942
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 20:06:27 +1000

    Create roomManager.js

---
+++ roomManager.js
@@ -0,0 +1,10 @@
+function manageRoom(room) {
+    // Logic for setting up basic infrastructure
+    if (!room.controller.my) {
+        // Logic to send claimers
+    } else {
+        // Logic to set up spawn, extensions, and defenses
+    }
+}
+
+module.exports = { manageRoom };
