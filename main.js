d392a71de7d8886948cb7642a3a377625331e841
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 20:01:48 +1000

    Update main.js

--- main.js
+++ main.js
@@ -19,32 +19,6 @@ module.exports.loop = function () {
     // Spawn new creeps based on priorities
     const workers = _.filter(Game.creeps, (creep) => creep.memory.role === 'worker');
     const defenders = _.filter(Game.creeps, (creep) => creep.memory.role === 'defender');
-    const claimers = _.filter(Game.creeps, (creep) => creep.memory.role === 'Let's correct the final `main.js` script and ensure proper functionality.
-
-### Correct `main.js`
-
-```javascript
-const { assignTasks } = require('taskManager');
-const towers = require('towers');
-
-// Constants for creep counts
-const WORKER_COUNT = 6;
-const DEFENDER_COUNT = 2;
-const CLAIMER_COUNT = 1;
-const MINIMUM_ENERGY_RESERVE = 300;
-
-module.exports.loop = function () {
-    // Clear memory of dead creeps
-    for (let name in Memory.creeps) {
-        if (!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
-        }
-    }
-
-    // Spawn new creeps based on priorities
-    const workers = _.filter(Game.creeps, (creep) => creep.memory.role === 'worker');
-    const defenders = _.filter(Game.creeps, (creep) => creep.memory.role === 'defender');
     const claimers = _.filter(Game.creeps, (creep) => creep.memory.role === 'claimer');
 
     if (Game.spawns['Spawn1'].room.energyAvailable >= MINIMUM_ENERGY_RESERVE) {
