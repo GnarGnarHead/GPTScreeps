5ae11a71b6bf3d11cbe55671c00e2be95f44ffa0
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 22:37:52 +1000

    Update worker.js

--- worker.js
+++ worker.js
@@ -14,51 +14,44 @@ function run(creep) {
     if (creep.memory.working) {
         manageConstructionAndRepairs(creep);
     } else {
-        harvestAndHaul(creep);
+        if (creep.memory.hauling) {
+            haulResources(creep);
+        } else {
+            harvestResources(creep);
+        }
     }
 }
 
-function harvestAndHaul(creep) {
-    if (creep.store.getFreeCapacity() > 0) {
-        // Harvest resources
-        const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-        if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
-            moveTo(creep, source);
-        }
+function harvestResources(creep) {
+    const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+    if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
+        moveTo(creep, source);
     } else {
-        // Haul resources
-        const nearestContainer = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: (structure) => structure.structureType === STRUCTURE_CONTAINER &&
-                                   structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+        // Drop off resources at the nearest container or storage
+        const dropOff = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => (structure.structureType === STRUCTURE_CONTAINER ||
+                                    structure.structureType === STRUCTURE_STORAGE) &&
+                                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+        });
+        if (dropOff && creep.transfer(dropOff, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            moveTo(creep, dropOff);
+        }
+    }
+}
+
+function haulResources(creep) {
+    if (creep.store[RESOURCE_ENERGY] === 0) {
+        creep.memory.hauling = false;
+    } else {
+        const dropOff = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => (structure.structureType === STRUCTURE_SPAWN ||
+                                    structure.structureType === STRUCTURE_EXTENSION ||
+                                    structure.structureType === STRUCTURE_STORAGE) &&
+                                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
         });
 
-        if (nearestContainer) {
-            if (creep.transfer(nearestContainer, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                moveTo(creep, nearestContainer);
-            }
-        } else {
-            const storage = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => (structure.structureType === STRUCTURE_SPAWN ||
-                                        structure.structureType === STRUCTURE_EXTENSION ||
-                                        structure.structureType === STRUCTURE_STORAGE) &&
-                                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-            });
-
-            if (storage) {
-                if (creep.transfer(storage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    moveTo(creep, storage);
-                }
-            } else {
-                const nextWorker = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
-                    filter: (c) => c.memory.role === 'worker' && c.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-                });
-
-                if (nextWorker) {
-                    if (creep.transfer(nextWorker, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                        moveTo(creep, nextWorker);
-                    }
-                }
-            }
+        if (dropOff && creep.transfer(dropOff, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            moveTo(creep, dropOff);
         }
     }
 }
