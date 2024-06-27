ecf69e42a66fae359d847fe18d5878ebbbe8bb4c
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 22:52:14 +1000

    Update worker.js

--- worker.js
+++ worker.js
@@ -14,48 +14,28 @@ function run(creep) {
     if (creep.memory.working) {
         manageConstructionAndRepairs(creep);
     } else {
-        if (creep.memory.role === 'harvester') {
-            harvestResources(creep);
-        } else if (creep.memory.role === 'transporter') {
-            transportResources(creep);
-        }
+        harvestAndTransport(creep);
     }
 }
 
-function harvestResources(creep) {
-    const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-    if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
-        moveTo(creep, source);
-    } else {
-        const container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: (structure) => structure.structureType === STRUCTURE_CONTAINER &&
-                                   structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-        });
-        if (container && creep.transfer(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-            moveTo(creep, container);
-        }
-    }
-}
-
-function transportResources(creep) {
-    if (creep.store[RESOURCE_ENERGY] === 0) {
-        const container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: (structure) => structure.structureType === STRUCTURE_CONTAINER &&
-                                   structure.store[RESOURCE_ENERGY] > 0
-        });
-        if (container && creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-            moveTo(creep, container);
+function harvestAndTransport(creep) {
+    if (creep.store.getFreeCapacity() > 0) {
+        // Harvest resources
+        const source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+        if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
+            moveTo(creep, source);
         }
     } else {
-        const storage = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        // Transport resources to storage or structures
+        const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
             filter: (structure) => (structure.structureType === STRUCTURE_SPAWN ||
                                     structure.structureType === STRUCTURE_EXTENSION ||
                                     structure.structureType === STRUCTURE_STORAGE) &&
                                     structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
         });
 
-        if (storage && creep.transfer(storage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-            moveTo(creep, storage);
+        if (target && creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            moveTo(creep, target);
         }
     }
 }
