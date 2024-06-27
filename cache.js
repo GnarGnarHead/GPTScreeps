119b259c1298afc80ed94cb08ab1db082cbc2b7f
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 18:42:22 +1000

    Create cache.js

---
+++ cache.js
@@ -0,0 +1,14 @@
+const pathCache = {};
+
+function getPath(fromPos, toPos) {
+    const key = fromPos.roomName + fromPos.x + fromPos.y + toPos.roomName + toPos.x + toPos.y;
+    if (pathCache[key]) {
+        return pathCache[key];
+    } else {
+        const path = fromPos.findPathTo(toPos, { ignoreCreeps: true });
+        pathCache[key] = path;
+        return path;
+    }
+}
+
+module.exports = { getPath };
