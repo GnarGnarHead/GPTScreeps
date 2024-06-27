193f3a50af31e8ad7a8f3aa73029e1d38d0350be
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 20:31:15 +1000

    Update roomManager.js

--- roomManager.js
+++ roomManager.js
@@ -15,19 +15,39 @@ function manageRoom(room) {
 }
 
 function createConstructionSites(room) {
-    // Example: Create construction sites for extensions around the spawn
-    let spawn = room.find(FIND_MY_SPAWNS)[0];
-    if (spawn) {
-        let positions = [
-            { x: spawn.pos.x - 1, y: spawn.pos.y - 1 },
-            { x: spawn.pos.x - 1, y: spawn.pos.y + 1 },
-            { x: spawn.pos.x + 1, y: spawn.pos.y - 1 },
-            { x: spawn.pos.x + 1, y: spawn.pos.y + 1 },
-        ];
-        for (let pos of positions) {
-            room.createConstructionSite(pos.x, pos.y, STRUCTURE_EXTENSION);
+    try {
+        const extensionCount = _.filter(Game.structures, (structure) => structure && structure.structureType === STRUCTURE_EXTENSION && structure.room.name === room.name).length;
+        const extensionSitesCount = _.filter(Game.constructionSites, (site) => site && site.structureType === STRUCTURE_EXTENSION && site.room.name === room.name).length;
+        const maxExtensions = CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][room.controller.level];
+
+        const containerCount = _.filter(Game.structures, (structure) => structure && structure.structureType === STRUCTURE_CONTAINER && structure.room.name === room.name).length;
+        const containerSitesCount = _.filter(Game.constructionSites, (site) => site && site.structureType === STRUCTURE_CONTAINER && site.room.name === room.name).length;
+        const maxContainers = CONTROLLER_STRUCTURES[STRUCTURE_CONTAINER][room.controller.level];
+
+        if (extensionCount + extensionSitesCount < maxExtensions) {
+            for (let x = room.controller.pos.x - 5; x <= room.controller.pos.x + 5; x++) {
+                for (let y = room.controller.pos.y - 5; y <= room.controller.pos.y + 5; y++) {
+                    if (room.createConstructionSite(x, y, STRUCTURE_EXTENSION) === OK) {
+                        return true;
+                    }
+                }
+            }
         }
+
+        if (containerCount + containerSitesCount < maxContainers) {
+            for (let x = room.controller.pos.x - 5; x <= room.controller.pos.x + 5; x++) {
+                for (let y = room.controller.pos.y - 5; y <= room.controller.pos.y + 5; y++) {
+                    if (room.createConstructionSite(x, y, STRUCTURE_CONTAINER) === OK) {
+                        return true;
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        console.log('Error in createConstructionSites:', error);
     }
+
+    return false;
 }
 
 module.exports = { manageRoom };
