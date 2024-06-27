3b11ca0d8c918edf85e5626b548964ec7ce08dc1
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 16:29:44 +1000

    Update builder.js

--- builder.js
+++ builder.js
@@ -16,17 +16,19 @@ function runBuilder(creep) {
     }
 
     if (creep.memory.working) {
-        let target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-        if (target) {
-            if (creep.build(target) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
-            }
-        } else {
-            let repairTarget = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
-            });
-            if (repairTarget && creep.repair(repairTarget) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(repairTarget, { visualizePathStyle: { stroke: '#ffaa00' } });
+        if (createConstructionSite(creep.room)) {
+            let target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+            if (target) {
+                if (creep.build(target) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
+                }
+            } else {
+                let repairTarget = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: (structure) => structure.hits < structure.hitsMax
+                });
+                if (repairTarget && creep.repair(repairTarget) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(repairTarget, { visualizePathStyle: { stroke: '#ffaa00' } });
+                }
             }
         }
     } else {
@@ -35,6 +37,29 @@ function runBuilder(creep) {
             creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
         }
     }
+}
+
+/**
+ * Function to create a construction site for an extension if needed
+ *
+ * @param {Room} room - The room to create the construction site in.
+ * @returns {boolean} - Returns true if a construction site was created, otherwise false.
+ */
+function createConstructionSite(room) {
+    const extensionCount = _.filter(Game.structures, (structure) => structure.structureType === STRUCTURE_EXTENSION && structure.room.name === room.name).length;
+    const extensionSitesCount = _.filter(Game.constructionSites, (site) => site.structureType === STRUCTURE_EXTENSION && site.room.name === room.name).length;
+    const maxExtensions = CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][room.controller.level];
+
+    if (extensionCount + extensionSitesCount < maxExtensions) {
+        for (let x = room.controller.pos.x - 5; x <= room.controller.pos.x + 5; x++) {
+            for (let y = room.controller.pos.y - 5; y <= room.controller.pos.y + 5; y++) {
+                if (room.createConstructionSite(x, y, STRUCTURE_EXTENSION) === OK) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 module.exports = { run: runBuilder };
