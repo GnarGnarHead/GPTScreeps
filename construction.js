b9eb909844e57e3c57f9f34fc4e62614e7514ae4
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 21:06:07 +1000

    Update construction.js

--- construction.js
+++ construction.js
@@ -1,15 +1,6 @@
 const { getPath } = require('cache');
 
 function manageConstructionAndRepairs(creep) {
-    if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
-        creep.memory.working = false;
-        creep.say('🔄 harvest');
-    }
-    if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
-        creep.memory.working = true;
-        creep.say('🚧 work');
-    }
-
     if (creep.memory.working) {
         let target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
         if (target) {
@@ -33,12 +24,6 @@ function manageConstructionAndRepairs(creep) {
                     creep.moveByPath(path);
                 }
             }
-        }
-    } else {
-        let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-        if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
-            const path = getPath(creep.pos, source.pos);
-            creep.moveByPath(path);
         }
     }
 }
