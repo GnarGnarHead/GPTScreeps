97046a619c4468c8b1ed8758711ad4e6d98bfd37
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 21:02:56 +1000

    Update construction.js

--- construction.js
+++ construction.js
@@ -1,33 +1,46 @@
-function createConstructionSites(room) {
-    const extensionCount = _.filter(Game.structures, (structure) => structure.structureType === STRUCTURE_EXTENSION && structure.room.name === room.name).length;
-    const extensionSitesCount = _.filter(Game.constructionSites, (site) => site.structureType === STRUCTURE_EXTENSION && site.room.name === room.name).length;
-    const maxExtensions = CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][room.controller.level];
+const { getPath } = require('cache');
 
-    const containerCount = _.filter(Game.structures, (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.room.name === room.name).length;
-    const containerSitesCount = _.filter(Game.constructionSites, (site) => site.structureType === STRUCTURE_CONTAINER && site.room.name === room.name).length;
-    const maxContainers = CONTROLLER_STRUCTURES[STRUCTURE_CONTAINER][room.controller.level];
+function manageConstructionAndRepairs(creep) {
+    if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
+        creep.memory.working = false;
+        creep.say('🔄 harvest');
+    }
+    if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
+        creep.memory.working = true;
+        creep.say('🚧 work');
+    }
 
-    if (extensionCount + extensionSitesCount < maxExtensions) {
-        for (let x = room.controller.pos.x - 5; x <= room.controller.pos.x + 5; x++) {
-            for (let y = room.controller.pos.y - 5; y <= room.controller.pos.y + 5; y++) {
-                if (room.createConstructionSite(x, y, STRUCTURE_EXTENSION) === OK) {
-                    return true;
+    if (creep.memory.working) {
+        let target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        if (target) {
+            if (creep.build(target) === ERR_NOT_IN_RANGE) {
+                const path = getPath(creep.pos, target.pos);
+                creep.moveByPath(path);
+            }
+        } else {
+            let repairTarget = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
+            if (repairTarget) {
+                if (creep.repair(repairTarget) === ERR_NOT_IN_RANGE) {
+                    const path = getPath(creep.pos, repairTarget.pos);
+                    creep.moveByPath(path);
+                }
+            } else {
+                let controller = creep.room.controller;
+                if (controller && creep.upgradeController(controller) === ERR_NOT_IN_RANGE) {
+                    const path = getPath(creep.pos, controller.pos);
+                    creep.moveByPath(path);
                 }
             }
         }
-    }
-
-    if (containerCount + containerSitesCount < maxContainers) {
-        for (let x = room.controller.pos.x - 5; x <= room.controller.pos.x + 5; x++) {
-            for (let y = room.controller.pos.y - 5; y <= room.controller.pos.y + 5; y++) {
-                if (room.createConstructionSite(x, y, STRUCTURE_CONTAINER) === OK) {
-                    return true;
-                }
-            }
+    } else {
+        let source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+        if (source && creep.harvest(source) === ERR_NOT_IN_RANGE) {
+            const path = getPath(creep.pos, source.pos);
+            creep.moveByPath(path);
         }
     }
-
-    return false;
 }
 
-module.exports = { createConstructionSites };
+module.exports = { manageConstructionAndRepairs };
