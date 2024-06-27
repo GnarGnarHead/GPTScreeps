58204cc805bd11a0cf78272b5dbcdf2f562b2821
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 19:20:10 +1000

    Update taskManager.js

--- taskManager.js
+++ taskManager.js
@@ -1,24 +1,44 @@
-class TaskManager {
-    constructor() {
-        this.tasks = [];
+const worker = require('./roles/worker');
+const defender = require('./roles/defender');
+const claimer = require('./roles/claimer');
+
+function assignTasks() {
+    const tasks = [];
+    for (let name in Game.creeps) {
+        let creep = Game.creeps[name];
+        if (creep.memory.role === 'worker') {
+            tasks.push({ creep: creep, priority: getTaskPriority(creep) });
+        } else if (creep.memory.role === 'defender') {
+            defender.run(creep);
+        } else if (creep.memory.role === 'claimer') {
+            claimer.run(creep);
+        }
     }
 
-    addTask(priority, type, target) {
-        this.tasks.push({ priority, type, target });
-        this.tasks.sort((a, b) => a.priority - b.priority);
-    }
-
-    getTask() {
-        return this.tasks.shift();
-    }
-
-    hasTasks() {
-        return this.tasks.length > 0;
-    }
-
-    count() {
-        return this.tasks.length;
-    }
+    tasks.sort((a, b) => a.priority - b.priority);
+    tasks.forEach(task => worker.run(task.creep));
 }
 
-module.exports = new TaskManager();
+function getTaskPriority(creep) {
+    if (creep.store.getFreeCapacity() > 0) {
+        return 1; // Harvesting
+    } else if (creep.store[RESOURCE_ENERGY] > 0) {
+        let constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        if (constructionSite) {
+            return 2; // Building
+        }
+        let structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if (structure) {
+            return 3; // Repairing
+        }
+        let controller = creep.room.controller;
+        if (controller) {
+            return 4; // Upgrading
+        }
+    }
+    return 5; // Idle or Remote Mining
+}
+
+module.exports = { assignTasks };
