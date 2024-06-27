0ae624a724b3bcaa5f0966c7d7b529af408ba925
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 21:45:22 +1000

    Update movement.js

--- movement.js
+++ movement.js
@@ -3,8 +3,8 @@ function moveTo(creep, target, visualize = false) {
     creep.moveTo(target, pathStyle);
 }
 
-function say(creep, message, duration = 3) {
-    creep.say(message, duration);
+function say(creep, message) {
+    creep.say(message, true); // The message will only be visible for one tick
 }
 
 module.exports = { moveTo, say };
