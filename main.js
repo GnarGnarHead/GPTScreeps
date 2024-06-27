f82ab74ae2cda436be87d57ec1bb040854bf410b
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 15:27:10 +1000

    main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const harvester = require('./harvester');
 const upgrader = require('./upgrader');
 const builder = require('./builder');
+const hauler = require('./hauler');
 
 const HARVESTER_COUNT = 2;
 const UPGRADER_COUNT = 1;
 const BUILDER_COUNT = 1;
+const HAULER_COUNT = 2;
 
 module.exports.loop = function () {
-    // Clear memory of dead creeps
     for (let name in Memory.creeps) {
         if (!Game.creeps[name]) {
             delete Memory.creeps[name];
@@ -15,10 +16,10 @@ module.exports.loop = function () {
         }
     }
 
-    // Creep spawning logic
     const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
     const upgraders = _.filter(Game.creeps, (creep) => creep.memory.role === 'upgrader');
     const builders = _.filter(Game.creeps, (creep) => creep.memory.role === 'builder');
+    const haulers = _.filter(Game.creeps, (creep) => creep.memory.role === 'hauler');
 
     if (harvesters.length < HARVESTER_COUNT) {
         spawnCreep('harvester');
@@ -29,8 +30,10 @@ module.exports.loop = function () {
     if (builders.length < BUILDER_COUNT) {
         spawnCreep('builder');
     }
+    if (haulers.length < HAULER_COUNT) {
+        spawnCreep('hauler');
+    }
 
-    // Assign tasks based on role
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
         if (creep.memory.role === 'harvester') {
@@ -39,13 +42,19 @@ module.exports.loop = function () {
             upgrader.run(creep);
         } else if (creep.memory.role === 'builder') {
             builder.run(creep);
+        } else if (creep.memory.role === 'hauler') {
+            hauler.run(creep);
         }
     }
 };
 
-// Function to spawn creeps
 function spawnCreep(role) {
-    let body = [WORK, CARRY, MOVE];
+    let body;
+    if (role === 'hauler') {
+        body = [CARRY, CARRY, MOVE, MOVE]; // Adjust the body parts based on your needs and available energy
+    } else {
+        body = [WORK, CARRY, MOVE];
+    }
     let newName = role.charAt(0).toUpperCase() + role.slice(1) + Game.time;
     let spawnName = Game.spawns['Spawn1'];
 
