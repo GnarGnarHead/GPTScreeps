07dfb0d2dbfe4bad12b9d90dbd0db33ffbab3d45
GnarGnarHead <44654467+GnarGnarHead@users.noreply.github.com>
2024-06-27 20:08:31 +1000

    Update roomManager.js

--- roomManager.js
+++ roomManager.js
@@ -1,9 +1,32 @@
 function manageRoom(room) {
-    // Logic for setting up basic infrastructure
-    if (!room.controller.my) {
-        // Logic to send claimers
+    if (!room.controller || !room.controller.my) {
+        // Logic to send claimers if the room is not owned
+        let claimers = _.filter(Game.creeps, (creep) => creep.memory.role === 'claimer' && creep.memory.target === room.name);
+        if (claimers.length === 0) {
+            let spawn = _.find(Game.spawns, (s) => s.room.energyAvailable >= 650);
+            if (spawn) {
+                spawn.spawnCreep([CLAIM, MOVE], 'Claimer' + Game.time, { memory: { role: 'claimer', target: room.name } });
+            }
+        }
     } else {
-        // Logic to set up spawn, extensions, and defenses
+        // Logic to set up spawn, extensions, and defenses if the room is owned
+        createConstructionSites(room);
+    }
+}
+
+function createConstructionSites(room) {
+    // Example: Create construction sites for extensions around the spawn
+    let spawn = room.find(FIND_MY_SPAWNS)[0];
+    if (spawn) {
+        let positions = [
+            { x: spawn.pos.x - 1, y: spawn.pos.y - 1 },
+            { x: spawn.pos.x - 1, y: spawn.pos.y + 1 },
+            { x: spawn.pos.x + 1, y: spawn.pos.y - 1 },
+            { x: spawn.pos.x + 1, y: spawn.pos.y + 1 },
+        ];
+        for (let pos of positions) {
+            room.createConstructionSite(pos.x, pos.y, STRUCTURE_EXTENSION);
+        }
     }
 }
 
